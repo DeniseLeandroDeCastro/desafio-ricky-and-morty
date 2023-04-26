dbf4df9d27f0f93e1d8a9c66aa971bf031c98706
Denise Castro <dmaria.natal@gmail.com>
2023-04-26 13:44:37 -0300

    [FEATURE-ANDROID][API-RICK-MORTY] - Inicia o repositório

---
+++ app/src/main/java/br/com/denisecastro/desafiorickandmorty/view/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.denisecastro.desafiorickandmorty.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.denisecastro.desafiorickandmorty.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
